c8542037e0029d8f00f1a748dc580bfc9959ad7b
Artefas <pasha777kot@mail.ru>
2018-07-15 22:12:40 +0300

    сохранение нароботок по навигационной панели

--- aggregator/ask/views.py
+++ aggregator/ask/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def main(request):
-    return render(request,'ask/main.html')
+    user_auth = request.GET.get("user")
+    return render(request,'ask/main.html', context={"user":user_auth})
